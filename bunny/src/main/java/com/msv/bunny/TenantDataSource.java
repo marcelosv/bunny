cd4d37d6f63f8627ca218bed99380d6620da4a27
Marcelo <marcelo@ceossistemas.com.br>
2016-05-21 15:03:33 -0300

    correção para reconectar banco

--- bunny/src/main/java/com/msv/bunny/TenantDataSource.java
+++ bunny/src/main/java/com/msv/bunny/TenantDataSource.java
@@ -1,18 +1,18 @@
 package com.msv.bunny;
 
 import java.io.Serializable;
+import java.sql.Driver;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Component;
 
 import com.msv.bunny.core.config.DataSourceConfig;
@@ -24,93 +24,117 @@ public class TenantDataSource implements Serializable {
 
 	@Autowired
 	private ConfigTenant configTenant;
-	
-    private HashMap<String, DataSource> dataSources = new HashMap<>();
 
-//    @Value("${spring.jpa.properties.hibernate.hbm2ddl.import_files}")
-//    private String importFiles;
-    
-    //@Autowired
-    //private DataSourceConfigRepository configRepo;
+	private HashMap<String, DataSource> dataSources = new HashMap<>();
 
-    /**
-     * DA UM GET NO DATASOURCE, CASO NAO EXISTE, ELE CRIAR UM NOVO.
-     * @param name
-     * @return
-     */
-    public DataSource getDataSource(String name) {
-    	
-    	// PEGA O DATASOURCE JA EXISTENTE
-        if (dataSources.get(name) != null) {
-            return dataSources.get(name);
-        }
-        
-        // SE NAO EXISTE, CRIA UM NOVO
-        DataSource dataSource = createDataSource(name);
-        if (dataSource != null) {
-            dataSources.put(name, dataSource);
-        }
-        return dataSource;
-    }
+	// @Value("${spring.jpa.properties.hibernate.hbm2ddl.import_files}")
+	// private String importFiles;
 
-    /**
-     * RETORNA TODOS OS DATASOURCES CARREGADO DO BANCO
-     * @return
-     */
-    @SuppressWarnings("unchecked")
+	// @Autowired
+	// private DataSourceConfigRepository configRepo;
+
+	/**
+	 * DA UM GET NO DATASOURCE, CASO NAO EXISTE, ELE CRIAR UM NOVO.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public DataSource getDataSource(String name) {
+
+		// PEGA O DATASOURCE JA EXISTENTE
+		if (dataSources.get(name) != null) {
+			return dataSources.get(name);
+		}
+
+		// SE NAO EXISTE, CRIA UM NOVO
+		DataSource dataSource = createDataSource(name);
+		if (dataSource != null) {
+			dataSources.put(name, dataSource);
+		}
+		return dataSource;
+	}
+
+	/**
+	 * RETORNA TODOS OS DATASOURCES CARREGADO DO BANCO
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
 	public Map<String, DataSource> getAll() {
-        List<DataSourceConfig> configList = configTenant.getDataSourceConfigRepository().findAll();
-        Map<String, DataSource> result = new HashMap<>();
-        for (DataSourceConfig config : configList) {
-            DataSource dataSource = getDataSource(config.getName());
-            result.put(config.getName(), dataSource);
-        }
-        return result;
-    }
+		List<DataSourceConfig> configList = configTenant.getDataSourceConfigRepository().findAll();
+		Map<String, DataSource> result = new HashMap<>();
+		for (DataSourceConfig config : configList) {
+			DataSource dataSource = getDataSource(config.getName());
+			result.put(config.getName(), dataSource);
+		}
+		return result;
+	}
 
-    /**
-     * 
-     * CRIA UM NOVO DATASOURCE
-     * 
-     * @param name
-     * @return
-     */
-    private DataSource createDataSource(String name) {
-        DataSourceConfig config = configTenant.getDataSourceConfigRepository().findByName(name);
-        if (config != null) {
-            DataSourceBuilder factory = DataSourceBuilder
-                    .create().driverClassName(config.getDriverClassName())
-                    .username(config.getUsername())
-                    .password(config.getPassword())
-                    .url(config.getUrl());
-            DataSource ds = factory.build();
-            
-            /*if (config.getInitialize()) {
-                initialize(ds);
-            }*/
-            
-            return ds;
-        }
-        return null;
-    }
+	/**
+	 * 
+	 * CRIA UM NOVO DATASOURCE
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private DataSource createDataSource(String name) {
+		DataSourceConfig config = configTenant.getDataSourceConfigRepository().findByName(name);
+		if (config != null) {
 
-    /**
-     * CARREGA NO DS CRIADO OS SCRIPTS DO CARICA
-     * @param dataSource
-     */
-    private void initialize(DataSource dataSource) {
-    	
-//    	if(StringUtils.isEmpty(importFiles)){
-//    		return;
-//    	}
-//    	
-//    	ClassPathResource importResource = new ClassPathResource(importFiles);
-    	
-//        ClassPathResource schemaResource = new ClassPathResource("schema.sql");
-//        ClassPathResource dataResource = new ClassPathResource("data.sql");
-//        ResourceDatabasePopulator populator = new ResourceDatabasePopulator(importResource);
-//        populator.execute(dataSource);
-    }
+			final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+			try {
+				ds.setDriver((Driver) Class.forName(config.getDriverClassName()).newInstance());
+			} catch (Exception e) {
+				throw new RuntimeException("Não pode achar o driver do banco.");
+			}
+			
+			ds.setUrl(config.getUrl());
+			ds.setUsername(config.getUsername());
+			ds.setPassword(config.getPassword());
 
+			Properties properties = new Properties();
+			properties.put("spring.datasource.testWhileIdle","true");
+			properties.put("spring.datasource.validationQuery","SELECT 1");
+			properties.put("spring.datasource.max-wait", "10000");
+			properties.put("spring.datasource.test-on-borrow","true");
+			ds.setConnectionProperties(properties);
+
+			/*
+			 * DataSourceBuilder factory = DataSourceBuilder
+			 * .create().driverClassName(config.getDriverClassName())
+			 * .username(config.getUsername()) .password(config.getPassword())
+			 * .url(config.getUrl()); DataSource ds = factory.build();
+			 */
+
+			/*
+			 * if (config.getInitialize()) { initialize(ds); }
+			 */
+
+			return ds;
+		}
+		return null;
+	}
+
+	/**
+	 * CARREGA NO DS CRIADO OS SCRIPTS DO CARICA
+	 * 
+	 * @param dataSource
+	 */
+	private void initialize(DataSource dataSource) {
+
+		// if(StringUtils.isEmpty(importFiles)){
+		// return;
+		// }
+		//
+		// ClassPathResource importResource = new
+		// ClassPathResource(importFiles);
+
+		// ClassPathResource schemaResource = new
+		// ClassPathResource("schema.sql");
+		// ClassPathResource dataResource = new ClassPathResource("data.sql");
+		// ResourceDatabasePopulator populator = new
+		// ResourceDatabasePopulator(importResource);
+		// populator.execute(dataSource);
+	}
 
 }
